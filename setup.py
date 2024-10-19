d539d2d4ace8905ae13dc93a18ce6f02493722a7
fanwc <354002894@qq.com>
2024-10-20 00:48:38 +0800

    fanwc

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dGbyG",
+    version="1.1",
+    author="Fan Wenchao",
+    author_email="12133024@mail.sustech.edu.cn",
+    description="dGbyG",
+    url="https://gitee.com/f-wc/dGbyG", 
+
+    packages=find_packages(), 
+)
